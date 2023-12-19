f4dbd242aba89dedd1a0a0742115c5729c2ae670
Carlos Barahona <carlos_nscl@hotmail.com>
2023-12-19 01:26:31 -0300

    More setup

--- src/app/problems/p01/p01.component.ts
+++ src/app/problems/p01/p01.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,8 +6,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './p01.component.html',
-  styleUrl: './p01.component.css'
+  styleUrl: './p01.component.css',
 })
 export class P01Component {
+
+  input: string | undefined;
+
+  constructor(private httpClient: HttpClient) {
+
+    this.httpClient.get("assets/inputs/p01.txt", {responseType: 'text'}).subscribe(
+      data => {
+        this.input = data;
+        console.log(this.input);
+      }
+    )
+
+  }
 
 }
