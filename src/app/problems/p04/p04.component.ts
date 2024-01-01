eb34eff21b2ba1984d02233cc590ce2ca83ac3f8
Carlos Barahona <carlos_nscl@hotmail.com>
2024-01-01 18:13:40 -0300

    Day 4 part 2

--- src/app/problems/p04/p04.component.ts
+++ src/app/problems/p04/p04.component.ts
@@ -5,7 +5,9 @@ interface Card {
   id: number;
   winners: number[];
   received: number[];
+  matchCount: number,
   scorePart1: number;
+  amount: number; // part 2
 }
 
 @Component({
@@ -20,6 +22,7 @@ export class P04Component {
   input = "";
   cards: Card[] = [];
   part1FinalSum = 0;
+  part2FinalSum = 0;
 
   constructor(private httpClient: HttpClient) {
 
@@ -28,18 +31,35 @@ export class P04Component {
         this.input = data;
         this.cards = this.parseCards(this.input);
         this.part1FinalSum = this.finishgHIMMMimeanpart1(this.cards);
+        this.executePart2idk(this.cards);
+        this.part2FinalSum = this.finishgHIMMMimeanpart2(this.cards);
       }
     );
+  }
+
+  sortNumArray(array: number[]) {
+    return array.sort((n1,n2) => n1 - n2);
   }
 
   parseCards(input: string): Card[] {
     const finalCards: Card[] = [];
 
+    const startWinners = 10;
+    const startPlayer = 42;
+
     const inputLines = input.trimEnd().split("\n");
     for (let line of inputLines) {
-      const id = Number(line.substring(5, 8));
-      const winnerNums = line.substring(10, 39).split(" ").filter( item => {return item !== ""}).map( item => {return Number(item)} );
-      const playerNumbers = line.substring(42).split(" ").filter( item => {return item !== ""}).map( item => {return Number(item)} );
+      const id = Number(line.substring(5, 8).replace(":", ""));
+      const winnerNums = line
+        .substring(startWinners, startPlayer - 3)
+        .split(" ")
+        .filter( item => {return item !== ""} )
+        .map( item => {return Number(item)} );
+      const playerNumbers = line
+        .substring(startPlayer)
+        .split(" ")
+        .filter( item => {return item !== ""} )
+        .map( item => {return Number(item)} );
 
       let cardScore = 0;
       let matchCount = 0;
@@ -57,8 +77,10 @@ export class P04Component {
         id: id,
         winners: winnerNums,
         received: playerNumbers,
+        matchCount: matchCount,
         scorePart1: cardScore,
-      })
+        amount: 1,
+      });
 
     }
 
@@ -71,6 +93,32 @@ export class P04Component {
 
     for (let card of cards) {
       sum += card.scorePart1;
+    }
+
+    return sum;
+  }
+
+  executePart2idk(cards: Card[]) {
+
+    for (let card of cards) {
+      for (let i = 1; i <= card.matchCount; i++) {
+        const cardToUpdate = cards.find( cardInArray => { return cardInArray.id === card.id + i } );
+        if (!cardToUpdate) {
+          console.warn("Didn't find card to add amount with (calculated) ID:", card.id + i);
+          continue;
+        }
+
+        cardToUpdate.amount += card.amount;
+      }
+    }
+  }
+
+  finishgHIMMMimeanpart2(cards: Card[]): number {
+
+    let sum = 0;
+
+    for (let card of cards) {
+      sum += card.amount;
     }
 
     return sum;
